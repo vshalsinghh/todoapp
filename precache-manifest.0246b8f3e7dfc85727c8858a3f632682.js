749aca22a50a94a71e3d47b1bec787a1855f205a
xCvishal <vishalsingh2402@gmail.com>
2020-10-12 11:46:51 +0530

    Updates

---
+++ precache-manifest.0246b8f3e7dfc85727c8858a3f632682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40a421ecc7d21062582b6e78b1fb129",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "b0366075c835270cb516",
+    "url": "/todoapp/static/css/main.64663519.chunk.css"
+  },
+  {
+    "revision": "58454ebc26f7f16e2e64",
+    "url": "/todoapp/static/js/2.80ec65a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.80ec65a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0366075c835270cb516",
+    "url": "/todoapp/static/js/main.6a2689e5.chunk.js"
+  },
+  {
+    "revision": "5a801258d7bf2526530b",
+    "url": "/todoapp/static/js/runtime-main.3e0c5687.js"
+  }
+]);
